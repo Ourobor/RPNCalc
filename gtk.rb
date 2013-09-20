3081e37c4f3ea54531d89550f07350c43a3c0323
Robert Lowe <rob@lowefrequency.com>
2013-09-20 13:48:57 -0400

    Started work on GTK interface :DDDDDDDDDDDDDD

---
+++ gtk.rb
@@ -0,0 +1,34 @@
+require 'gtk3'
+
+class RubyApp < Gtk::Window
+	def initialize
+		super
+		
+		set_title "Hello World!"
+		
+		mainStack = Gtk::TextView.new
+		commandStack = Gtk::TextView.new
+		variableTable = Gtk::TextView.new
+		
+		uiBox = Gtk::Box.new :vertical, 0
+		entry = Gtk::Entry.new
+		errorstuff = Gtk::TextView.new
+		uiBox.add errorstuff, :expand => true, :fill => true, :padding => 1
+		uiBox.add entry, :expand => false, :fill => false, :padding => 1
+		
+		mainTable = Gtk::Table.new 4,3,true
+		mainTable.set_row_spacings 1
+		mainTable.set_column_spacings 1
+		mainTable.attach(mainStack, 0,1,0,3)
+		mainTable.attach(commandStack, 1,2,0,3)
+		mainTable.attach(variableTable, 2,3,0,3)
+		mainTable.attach(uiBox,0,3,3,4)
+		
+		add mainTable
+
+		show_all
+	end
+end
+Gtk.init
+	window = RubyApp.new
+Gtk.main
