9555a7109a968ebeecc9a2e8ded7806871a4d310
Robert Lowe <rob@lowefrequency.com>
2013-09-21 16:31:04 -0400

    OMG GTK window kinda works a little
    The main stack wiget actually displays the stack and the command line sends the commands to be parsed!

--- gtk.rb
+++ gtk.rb
@@ -1,4 +1,6 @@
 require 'gtk3'
+require './commands'
+require './stack'
 
 class RubyApp < Gtk::Window
 	def initialize
@@ -10,27 +12,47 @@ class RubyApp < Gtk::Window
 			Gtk.main_quit 
 		end
 	
-		mainStack = Gtk::TextView.new
-		commandStack = Gtk::TextView.new
-		variableTable = Gtk::TextView.new
+		@mainStack = Gtk::TextView.new
+		mainBuffer = @mainStack.buffer
+		@commandStack = Gtk::TextView.new
+		@variableTable = Gtk::TextView.new
 		
 		uiBox = Gtk::Box.new :vertical, 0
-		entry = Gtk::Entry.new
-		errorstuff = Gtk::TextView.new
-		uiBox.add entry, :expand => false, :fill => false, :padding => 1
-		uiBox.add errorstuff, :expand => true, :fill => true, :padding => 1
+		@entry = Gtk::Entry.new
+		@entry.signal_connect "activate" do |s,e|
+			Commands::CB.parse(@entry.text)
+			@entry.text = ""
+			
+			changeState
+		end
+		@errorstuff = Gtk::TextView.new
+		uiBox.add @entry, :expand => false, :fill => false, :padding => 1
+		uiBox.add @errorstuff, :expand => true, :fill => true, :padding => 1
 		
 		mainTable = Gtk::Table.new 4,3,true
 		mainTable.set_row_spacings 1
 		mainTable.set_column_spacings 1
-		mainTable.attach(mainStack, 0,1,0,3)
-		mainTable.attach(commandStack, 1,2,0,3)
-		mainTable.attach(variableTable, 2,3,0,3)
+		mainTable.attach(@mainStack, 0,1,0,3)
+		mainTable.attach(@commandStack, 1,2,0,3)
+		mainTable.attach(@variableTable, 2,3,0,3)
 		mainTable.attach(uiBox,0,3,3,4)
-		
+		changeState	
 		add mainTable
 
 		show_all
+	end
+	def changeState
+		#regen buffers and such
+		mainBuffer = @mainStack.buffer
+		mainBuffer.set_text ""
+		textiter = mainBuffer.start_iter
+		itter = Stack.S.itter
+		
+		while itter.done? == false
+			mainBuffer.insert(textiter, itter.value.to_s + "\n")
+			itter.next
+		end
+		
 	end
 end
 Gtk.init
